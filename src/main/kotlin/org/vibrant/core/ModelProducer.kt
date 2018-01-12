85f94f06421d657001ed0bf7478c8b7a56a92beb
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-12 07:24:51 +0300

    Model serializer with generic base model class

--- src/main/kotlin/org/vibrant/core/ModelProducer.kt
+++ src/main/kotlin/org/vibrant/core/ModelProducer.kt
@@ -2,6 +2,6 @@ package org.vibrant.core
 
 import org.vibrant.core.models.Model
 
-abstract class ModelProducer<out T : Model> {
-    abstract fun produce(serializer: ModelSerializer): T
+abstract class ModelProducer<T : Model> {
+    abstract fun produce(serializer: ModelSerializer<T>): T
 }
--- src/main/kotlin/org/vibrant/core/producers/TransactionProducer.kt
+++ src/main/kotlin/org/vibrant/core/producers/TransactionProducer.kt
@@ -3,4 +3,4 @@ package org.vibrant.core.producers
 import org.vibrant.core.ModelProducer
 import org.vibrant.core.models.TransactionModel
 
-abstract class TransactionProducer<out T: TransactionModel>: ModelProducer<T>()
+abstract class TransactionProducer<T: TransactionModel>: ModelProducer<T>()
