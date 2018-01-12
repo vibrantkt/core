cc79bdf2141b068eaa21369348db786dd095767c
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-13 00:11:57 +0300

    blockchain new block listener

--- src/main/kotlin/org/vibrant/core/producers/BlockChainProducer.kt
+++ src/main/kotlin/org/vibrant/core/producers/BlockChainProducer.kt
@@ -2,5 +2,18 @@ package org.vibrant.core.producers
 
 import org.vibrant.core.ModelProducer
 import org.vibrant.core.models.BlockChainModel
+import org.vibrant.core.models.BlockModel
 
-abstract class BlockChainProducer<out T: BlockChainModel>: ModelProducer<T>()
+abstract class BlockChainProducer<out T: BlockChainModel>: ModelProducer<T>(){
+    private val onNewBlockListener = arrayListOf<NewBlockListener>()
+
+
+
+    fun addNewBlockListener(listener: NewBlockListener) = this.onNewBlockListener.add(listener)
+    fun removeNewBlockListener(listener: NewBlockListener) = this.onNewBlockListener.remove(listener)
+
+
+    abstract class NewBlockListener{
+        abstract fun nextBlock(blockModel: BlockModel)
+    }
+}
