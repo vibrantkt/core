07831ad80ba744afb882f6978280479d83a8bf25
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-12 18:24:19 +0300

    deprecated udp session peer

--- src/main/kotlin/org/vibrant/core/node/AbstractNode.kt
+++ src/main/kotlin/org/vibrant/core/node/AbstractNode.kt
@@ -29,6 +29,6 @@ abstract class AbstractNode<out T: BlockChainModel, out B: BlockChainProducer<T>
     /**
      * Connect to peer
      */
-    abstract suspend fun connect(remoteNode: RemoteNode): Boolean
+    abstract fun connect(remoteNode: RemoteNode): Boolean
 
 }
--- src/main/kotlin/org/vibrant/core/node/Session.kt
+++
@@ -1,5 +0,0 @@
-package org.vibrant.core.node
-
-abstract class Session<Package: UDPSessionPeer.Communication.CommunicationPackage>(val remoteNode: RemoteNode, val request: Package) {
-    abstract fun handle(response: Package)
-}
