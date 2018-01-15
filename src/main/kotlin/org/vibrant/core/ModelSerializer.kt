0e0eff802c130cdcda9c6ea17d6b59f1dbe44b8c
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-15 20:46:00 +0300

    generic model serializer

--- src/main/kotlin/org/vibrant/core/ModelSerializer.kt
+++ src/main/kotlin/org/vibrant/core/ModelSerializer.kt
@@ -2,7 +2,7 @@ package org.vibrant.core
 
 import org.vibrant.core.models.Model
 
-abstract class ModelSerializer {
-    abstract fun serialize(model: Model): ByteArray
-    abstract fun deserialize(serialized: ByteArray): Model
+abstract class ModelSerializer<T: Model> {
+    abstract fun serialize(model: T): ByteArray
+    abstract fun deserialize(serialized: ByteArray): T
 }
--- src/main/kotlin/org/vibrant/core/producers/BlockProducer.kt
+++ src/main/kotlin/org/vibrant/core/producers/BlockProducer.kt
@@ -3,4 +3,4 @@ package org.vibrant.core.producers
 import org.vibrant.core.ModelProducer
 import org.vibrant.core.models.Model
 
-abstract class BlockProducer<out T : Model>: ModelProducer<T>()
+abstract class BlockProducer<T : Model>: ModelProducer<T>()
