a4973ad54ac5acbf474c1f30bad58a097b11ba9b
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-30 23:56:46 +0300

    rpc extension and error handling in invoke

--- src/main/kotlin/org/vibrant/core/rpc/json/JSONRPCResponse.kt
+++ src/main/kotlin/org/vibrant/core/rpc/json/JSONRPCResponse.kt
@@ -1,8 +1,11 @@
 package org.vibrant.core.rpc.json
 
+import org.vibrant.core.rpc.RPCRequest
+import org.vibrant.core.rpc.RPCResponse
+
 data class JSONRPCResponse<out T>(
         val result: T?,
         val error: SimpleJSONRPCError?,
         val id: Long,
         val version: String = "2.0"
-): JSONRPCEntity(id)
+): JSONRPCEntity(id), RPCResponse
